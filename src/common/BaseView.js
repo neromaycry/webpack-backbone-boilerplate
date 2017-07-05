59cf9c34e6f603d9ab0c103ebc65715e3143710b
gjwlg <guojiawei@gsiworld.com.cn>
2017-07-06 00:14:20 +0800

    添加 BaseView中onCreate，onStart函数

--- src/common/BaseView.js
+++ src/common/BaseView.js
@@ -1,1 +1,24 @@
 var Backbone = require('Backbone');
+var $ = require('jquery');
+
+var BaseView = Backbone.View.extend({
+
+    initialize: function (args) {
+        var self = this;
+        this.onCreate();
+        $(document).ready(function () {
+            self.onStart();
+        });
+    },
+
+    onCreate: function () {
+        console.log('Page onCreate');
+    },
+
+    onStart: function () {
+        console.log('Page onStart');
+    },
+
+});
+
+module.exports = BaseView;
